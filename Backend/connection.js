9d451398e208412f7364f78adee635887bb51ef1
Nikitha-Pillai <nikitha.m.pillai@gmail.com>
2024-08-04 11:59:39 +0530

    final

--- Backend/connection.js
+++ Backend/connection.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb+srv://projdb:abcd%401234@cluster0.yoqqqcw.mongodb.net/final?retryWrites=true&w=majority&appName=Cluster0")  // Replace with your MongoDB URI and database name
+  .connect("mongodb+srv://projdb:abcd%401234@cluster0.yoqqqcw.mongodb.net/final?retryWrites=true&w=majority&appName=Cluster0")  
   .then(() => {
     console.log("Connected to DB");
   })
